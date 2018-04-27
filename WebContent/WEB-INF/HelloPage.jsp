f39d171a8b9d04e5e9fc40598dcac32ef3d542fb
Jeyaganeshan Jeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2018-04-27 15:15:19 +0530

    first commit

---
+++ WebContent/WEB-INF/HelloPage.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Miyaav</title>
+</head>
+<body>
+	<h1>First Spring MVC Application Demo</h1>
+	<h2>${welcomeMessage}</h2>
+</body>
+</html>
---
+++ src/com/jeya/springmvc/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.jeya.springmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController{
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		
+		modelAndView.addObject("welcomeMessage", "Hi user, welcome to the first Spring MVC Application");
+		return modelAndView;
+	}
+
+}
