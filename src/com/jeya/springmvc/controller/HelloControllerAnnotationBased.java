a80bf70e315c64c68cf9abf352d58d809d1e0992
Jeyaganeshan Jeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2018-04-27 17:29:26 +0530

    multi action controller class and class level request mapping

--- src/com/jeya/springmvc/controller/HelloControllerAnnotationBased.java
+++ src/com/jeya/springmvc/controller/HelloControllerAnnotationBased.java
@@ -5,13 +5,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/greet")
 public class HelloControllerAnnotationBased {
 
 	@RequestMapping("/welcomeByAnnotation")
-	protected ModelAndView helloWorld() throws Exception {
+	protected ModelAndView helloWorld() throws Exception { // a request handler
+															// method
 		ModelAndView modelAndView = new ModelAndView("HelloPage");
 
 		modelAndView.addObject("welcomeMessage", "Hi user, welcome to the first Spring MVC Application");
+		return modelAndView;
+	}
+
+	@RequestMapping("/hi") // now http://localhost:8090/FirstSpringMVCProject/greet/hi is needed to execute this method
+	protected ModelAndView hi() throws Exception {
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+
+		modelAndView.addObject("welcomeMessage", "Hi Hi Hi");
 		return modelAndView;
 	}
 }
