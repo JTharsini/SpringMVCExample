9899f7cd30ed66990a228a4a284a34a0a3a53039
Jeyaganeshan Jeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2018-05-01 13:40:27 +0530

    object type as modelAndView's addObject argument

--- WebContent/WEB-INF/view/AdmissionSuccess.jsp
+++ WebContent/WEB-INF/view/AdmissionSuccess.jsp
@@ -4,6 +4,8 @@
 </head>
 <body>
 	<h1>Congratulations!! Success!!</h1>
-	<h2>${msg}</h2>
+	<h2>${headerMessage}</h2>
+	<h3>Student Name : ${student.studentName}</h3>
+	<h3>Student Hobby : ${student.studentHobby}</h3>
 </body>
 </html>
--- src/com/jeya/springmvc/controller/StudentAdmissionController.java
+++ src/com/jeya/springmvc/controller/StudentAdmissionController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.jeya.springmvc.model.Student;
+
 @Controller
 public class StudentAdmissionController {
 	@RequestMapping(value = "/admissionForm.html", method = RequestMethod.GET)
@@ -29,8 +31,15 @@ public class StudentAdmissionController {
 	public ModelAndView submitAdmissionFormWithMap(@RequestParam Map<String, String> reqPar) {
 		String name = reqPar.get("studentName");
 		String hobby = reqPar.get("studentHobby");
+		
+		Student student = new Student();
+		student.setStudentName(name);
+		student.setStudentHobby(hobby);
+		
 		ModelAndView modelAndView = new ModelAndView("AdmissionSuccess");
-		modelAndView.addObject("msg", "Details submitted by you:: Name: " + name + ", Hobby: " + hobby);
+		
+		modelAndView.addObject("headerMessage", "Details submitted by you");
+		modelAndView.addObject("student", student);
 		return modelAndView;
 	}
 }
