aca07721d75e88d574dbbc26f6b2faa49881a7c5
Jeyaganeshan Jeyatharsini <jeyaganeshan.jeyatharsini@cambio.lk>
2018-05-08 16:48:43 +0530

    postHandle and afterCompletion method in interceptors

--- src/com/jeya/springmvc/springspeccustom/DayOfWeekBasedAccessInterceptor.java
+++ src/com/jeya/springmvc/springspeccustom/DayOfWeekBasedAccessInterceptor.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter{
@@ -23,5 +24,19 @@ public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter{
 			return false;
 		}
 		return true;
+	}
+	
+	@Override 
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception
+	{
+		// this method would be called after Spring MVC executes the request handler method for the request
+		System.out.println("HandlerInterceptorAdaptor : Spring MVC called postHandle method for " + request.getRequestURI().toString());
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception
+	{
+		// this method would be called after the response object is produced by the view for the request
+		System.out.println("HandlerInterceptorAdaptor : Spring MVC called afterCompletion method for " + request.getRequestURI().toString());
 	}
 }
