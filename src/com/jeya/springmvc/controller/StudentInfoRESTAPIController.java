7575575761f2232b2ff56ed25c557b3874adc64a
Jeyaganeshan Jeyatharsini <Jeyaganeshan.Jeyatharsini@cambio.lk>
2018-07-09 14:36:18 +0530

    Http PUT and @RequestBody annotation

--- src/com/jeya/springmvc/controller/StudentInfoRESTAPIController.java
+++ src/com/jeya/springmvc/controller/StudentInfoRESTAPIController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import com.jeya.springmvc.model.Student;
 //@Controller is also allowed
 @RestController
 public class StudentInfoRESTAPIController {
+	// get all students' record
 	@RequestMapping(value = "/students", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public List<Student> getStudentsList() {
 		Student student1 = new Student();
@@ -33,11 +35,24 @@ public class StudentInfoRESTAPIController {
 		return studentsList;
 	}
 
+	// get a student's record with the specified name
 	@RequestMapping(value = "/students/{name}", method = RequestMethod.GET)
 	public Student getStudent(@PathVariable("name") String studentName) {
 		Student student = new Student();
 		student.setStudentName(studentName);
 		student.setStudentHobby("WWE");
 		return student;
+	}
+	
+	// update a student record
+	@RequestMapping(value = "/students/{name}", method = RequestMethod.PUT)
+	public boolean updateStudent(@PathVariable("name") String studentName, @RequestBody Student student) {
+		System.out.println("Student Name: " + studentName);
+		System.out.println("Student's new name: " + student.getStudentName() + ", Student Hobby: " + student.getStudentHobby());
+		
+		// find the matching student record using "studentName" from the DB
+		// update the matching student record with the information of student sent by the client
+		// return true if update is successfully done and return false if update is not done successfully
+		return true;
 	}
 }
